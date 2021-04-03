dcb0f0a1443d4486071c3776e416ff75a81efe12
GraczykMateusz <43554417+GraczykMateusz@users.noreply.github.com>
2021-04-03 20:14:43 +0200

    add: alarm

--- SmartHome/Alarm.ino
+++ SmartHome/Alarm.ino
@@ -1,10 +1,38 @@
-#define ALARM 9
+static bool isAlarmOn = false;
 
 void turnOnAlarm() {
-  if(digitalRead(ALARM))
-      turnOnLightUpstairs();
+  isAlarmOn = true;
 }
 
 void turnOffAlarm() {
+  isAlarmOn = false;
   
+  digitalWrite(RED_ALARM, LOW);
+  digitalWrite(GREEN_ALARM, LOW);
+}
+
+bool getIsAlarmOn() {
+  return isAlarmOn;
+}
+
+void scanning() {
+  if(isDetectMove()) {
+    turnOnRedAlarm(); 
+  } else {
+    turnOnGreenAlarm();
+  }
+}
+
+bool isDetectMove() {
+  return digitalRead(ALARM);
+}
+
+void turnOnRedAlarm() {
+  digitalWrite(RED_ALARM, HIGH);
+  digitalWrite(GREEN_ALARM, LOW);
+}
+
+void turnOnGreenAlarm() {
+  digitalWrite(GREEN_ALARM, HIGH);
+  digitalWrite(RED_ALARM, LOW);
 }
